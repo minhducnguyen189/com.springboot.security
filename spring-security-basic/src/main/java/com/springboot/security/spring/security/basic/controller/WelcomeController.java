b1f8077cf5d24dcf4e44588997949c3a770e98cd
minhducnguyen189 <minhducnguyen189@gmail.com>
2022-02-27 15:42:40 +0700

    add new module for spring security basic

---
+++ spring-security-basic/src/main/java/com/springboot/security/spring/security/basic/controller/WelcomeController.java
@@ -0,0 +1,14 @@
+package com.springboot.security.spring.security.basic.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+
+    @GetMapping("/welcome")
+    public String sayWelcome() {
+        return "Welcome from spring application with security";
+    }
+
+}
