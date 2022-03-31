d91b04ef160ea758988feea271e5522bb56e9855
minhducnguyen189 <minhducnguyen189@gmail.com>
2022-03-31 21:17:58 +0700

    use inMemoryAuthentication to craete inMemory users

--- custom-basic-spring-security/src/main/java/com/springboot/security/custom/basic/spring/security/config/ProjectSecurityConfig.java
+++ custom-basic-spring-security/src/main/java/com/springboot/security/custom/basic/spring/security/config/ProjectSecurityConfig.java
@@ -1,8 +1,10 @@
 package com.springboot.security.custom.basic.spring.security.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 
 @Configuration
 public class ProjectSecurityConfig extends WebSecurityConfigurerAdapter {
@@ -28,6 +30,12 @@ public class ProjectSecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/v1/notice").permitAll()
                 .and().formLogin()
                 .and().httpBasic();
+    }
+
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication().withUser("admin").password("12345").authorities("admin")
+                .and().withUser("user").password("12345").authorities("read")
+                .and().passwordEncoder(NoOpPasswordEncoder.getInstance());
     }
 
 
