e8bc00c87b87e0dc7b2a3dccb7c20edff5525e29
minhducnguyen189 <minhducnguyen189@gmail.com>
2022-05-17 22:37:45 +0700

    use BCrypt for password encoder with custom userDetailService

--- custom-basic-spring-security/src/main/java/com/springboot/security/custom/basic/spring/security/config/ProjectSecurityConfig.java
+++ custom-basic-spring-security/src/main/java/com/springboot/security/custom/basic/spring/security/config/ProjectSecurityConfig.java
@@ -8,6 +8,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
@@ -64,9 +65,14 @@ public class ProjectSecurityConfig extends WebSecurityConfigurerAdapter {
 //        return new JdbcUserDetailsManager(dataSource);
 //    }
 
+//    @Bean
+//    public PasswordEncoder passwordEncoder() {
+//        return NoOpPasswordEncoder.getInstance();
+//    }
+
     @Bean
     public PasswordEncoder passwordEncoder() {
-        return NoOpPasswordEncoder.getInstance();
+        return new BCryptPasswordEncoder();
     }
 
 
