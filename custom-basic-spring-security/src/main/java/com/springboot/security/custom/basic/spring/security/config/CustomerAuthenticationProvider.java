5d01660ca2c27f59479b3b275b33fb58abf47271
minhducnguyen189 <minhducnguyen189@gmail.com>
2022-06-04 22:55:59 +0700

    add custom authentication provider

---
+++ custom-basic-spring-security/src/main/java/com/springboot/security/custom/basic/spring/security/config/CustomerAuthenticationProvider.java
@@ -0,0 +1,54 @@
+package com.springboot.security.custom.basic.spring.security.config;
+
+import com.springboot.security.custom.basic.spring.security.entity.CustomerEntity;
+import com.springboot.security.custom.basic.spring.security.repository.CustomerRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class CustomerAuthenticationProvider implements AuthenticationProvider {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(CustomerAuthenticationProvider.class);
+
+    @Autowired
+    private CustomerRepository customerRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Override
+    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+        LOGGER.info("CustomerAuthenticationProvider is triggered");
+        String username = authentication.getName();
+        String password = authentication.getCredentials().toString();
+        List<CustomerEntity> customerEntities = this.customerRepository.findByEmail(username);
+        if (customerEntities.size() > 0) {
+            if (passwordEncoder.matches(password, customerEntities.get(0).getPassword())) {
+                List<GrantedAuthority> authorities = new ArrayList<>();
+                authorities.add(new SimpleGrantedAuthority(customerEntities.get(0).getRole()));
+                return new UsernamePasswordAuthenticationToken(username, password, authorities);
+            } else {
+                throw new BadCredentialsException("Invalid Password");
+            }
+        }
+        throw new BadCredentialsException("No user registered with this details");
+    }
+
+    @Override
+    public boolean supports(Class<?> authentication) {
+        return authentication.equals(UsernamePasswordAuthenticationToken.class);
+    }
+}
--- custom-basic-spring-security/src/main/java/com/springboot/security/custom/basic/spring/security/service/BankUserDetailsService.java
+++ custom-basic-spring-security/src/main/java/com/springboot/security/custom/basic/spring/security/service/BankUserDetailsService.java
@@ -3,6 +3,8 @@ package com.springboot.security.custom.basic.spring.security.service;
 import com.springboot.security.custom.basic.spring.security.entity.CustomerEntity;
 import com.springboot.security.custom.basic.spring.security.model.SecurityCustomer;
 import com.springboot.security.custom.basic.spring.security.repository.CustomerRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -14,12 +16,14 @@ import java.util.List;
 @Service
 public class BankUserDetailsService implements UserDetailsService {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(BankUserDetailsService.class);
+
     @Autowired
     private CustomerRepository customerRepository;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-
+        LOGGER.info("BankUserDetailsService is triggered!");
         List<CustomerEntity> customerEntities = this.customerRepository.findByEmail(username);
 
         if (customerEntities.isEmpty()) {
