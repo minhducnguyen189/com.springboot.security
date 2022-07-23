9433147d7d3e0d7681172eb3e376c87921d03c92
DucNguyen <minhducnguyen189@gmail.com>
2022-07-23 20:51:34 +0700

    config logging for spring security

--- custom-basic-spring-security/src/main/java/com/springboot/security/custom/basic/spring/security/CustomDefaultSpringSecurityApplication.java
+++ custom-basic-spring-security/src/main/java/com/springboot/security/custom/basic/spring/security/CustomDefaultSpringSecurityApplication.java
@@ -3,9 +3,11 @@ package com.springboot.security.custom.basic.spring.security;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @SpringBootApplication
 @EnableJpaRepositories
+@EnableWebSecurity(debug = true)
 public class CustomDefaultSpringSecurityApplication {
 
     public static void main(String[] args) {
