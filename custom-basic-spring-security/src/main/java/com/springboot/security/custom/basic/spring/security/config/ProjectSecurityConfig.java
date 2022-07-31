4f7fa88eef2182f47f44bc35fbbe1432ab14ac05
DucNguyen <minhducnguyen189@gmail.com>
2022-07-31 09:06:46 +0700

    add RequestValidationBeforeFilter

--- custom-basic-spring-security/src/main/java/com/springboot/security/custom/basic/spring/security/config/ProjectSecurityConfig.java
+++ custom-basic-spring-security/src/main/java/com/springboot/security/custom/basic/spring/security/config/ProjectSecurityConfig.java
@@ -1,11 +1,13 @@
 package com.springboot.security.custom.basic.spring.security.config;
 
+import com.springboot.security.custom.basic.spring.security.filter.RequestValidationBeforeFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
@@ -32,7 +34,8 @@ public class ProjectSecurityConfig extends WebSecurityConfigurerAdapter {
                 .ignoringAntMatchers("/v1/user")
                 .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
                 .and().cors()
-                .and().authorizeRequests()
+                .and().addFilterBefore(new RequestValidationBeforeFilter(), BasicAuthenticationFilter.class)
+                .authorizeRequests()
 //                .antMatchers("/v1/user").authenticated()
 //                .antMatchers("/v1/accounts/**").authenticated()
 //                .antMatchers("/v1/balance").authenticated()
