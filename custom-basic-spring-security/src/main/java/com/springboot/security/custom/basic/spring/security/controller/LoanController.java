cd95ef571451438bae9579dff22ce9f0575100e3
DucNguyen <minhducnguyen189@gmail.com>
2022-08-24 22:11:30 +0700

    update api getLoansByCustomerID

--- custom-basic-spring-security/src/main/java/com/springboot/security/custom/basic/spring/security/controller/LoanController.java
+++ custom-basic-spring-security/src/main/java/com/springboot/security/custom/basic/spring/security/controller/LoanController.java
@@ -1,16 +1,30 @@
 package com.springboot.security.custom.basic.spring.security.controller;
 
+import com.springboot.security.custom.basic.spring.security.model.Loan;
+import com.springboot.security.custom.basic.spring.security.service.LoanService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.websocket.server.PathParam;
+import java.util.List;
+
 @RestController
 public class LoanController {
+
+    @Autowired
+    private LoanService loanService;
 
     @RequestMapping(method = RequestMethod.GET, path = "/v1/loan")
     public ResponseEntity<String> getLoanDetail() {
         return ResponseEntity.ok("This is the loan details");
+    }
+
+    @RequestMapping(method = RequestMethod.GET, path = "/v1/loan/customer/{customerId}")
+    public ResponseEntity<List<Loan>> getLoansByCustomerId(@PathParam("customerId") Integer customerId) {
+        return ResponseEntity.ok(this.loanService.getLoans(customerId));
     }
 
 }
