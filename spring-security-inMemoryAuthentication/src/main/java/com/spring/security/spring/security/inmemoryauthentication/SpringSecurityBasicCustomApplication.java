b8bc76318cf7f727666087c8af4b1ee7c17710d0
DucNguyen <minhducnguyen189@gmail.com>
2022-09-28 17:07:35 +0700

    add example for inmemoryauthentication

---
+++ spring-security-inMemoryAuthentication/src/main/java/com/spring/security/spring/security/inmemoryauthentication/SpringSecurityBasicCustomApplication.java
@@ -0,0 +1,13 @@
+package com.spring.security.spring.security.inmemoryauthentication;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityBasicCustomApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityBasicCustomApplication.class, args);
+    }
+}
---
+++ spring-security-inMemoryAuthentication/src/main/java/com/spring/security/spring/security/inmemoryauthentication/config/ProjectSecurityConfig.java
@@ -0,0 +1,41 @@
+package com.spring.security.spring.security.inmemoryauthentication.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+
+@Configuration
+public class ProjectSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    /**
+     *
+     * contact: Not Secure
+     * notice: Not Secure
+     * balance: Secure
+     * Card: Secure
+     * Loan: Secure
+     * Account: Secure
+     *
+     */
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/v1/accounts/**").authenticated()
+                .antMatchers("/v1/balance").authenticated()
+                .antMatchers("/v1/loan").authenticated()
+                .antMatchers("/v1/card").authenticated()
+                .antMatchers("/v1/contact").permitAll()
+                .antMatchers("/v1/notice").permitAll()
+                .and().formLogin()
+                .and().httpBasic();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication().withUser("admin").password("12345").authorities("admin")
+                .and().withUser("user").password("12345").authorities("read")
+                .and().passwordEncoder(NoOpPasswordEncoder.getInstance());
+    }
+}
