7a5be5cebdaa49e0ce996431985a1862abdfdd83
ducnguyen <minhducnguyen189@gmail.com>
2022-12-18 20:38:09 +0700

    remove redumdant imports

--- spring-security-oauth-client-credentials-grant-type/src/main/java/com/springboot/sprint/security/oauth/client/credentials/grant/type/config/ProjectSecurityConfig.java
+++ spring-security-oauth-client-credentials-grant-type/src/main/java/com/springboot/sprint/security/oauth/client/credentials/grant/type/config/ProjectSecurityConfig.java
@@ -4,7 +4,6 @@ import com.springboot.sprint.security.oauth.client.credentials.grant.type.consta
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
